c571b9ce0b0d0bf20f81b6bbccd5c3d8f3a40b21
none <none>
2022-04-28 11:17:01 +0300

    remove git module && add nuxt in git

---
+++ alfa-nuxt/components/Header.vue
@@ -0,0 +1,323 @@
+<template>
+	<header class="header">
+		<div class="container">
+			<div class="header__wrapper">
+				<div class="header__iner">
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M5 17H19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					<path d="M5 12H19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					<path d="M5 7H19" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					<path d="M15 15L21 21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+				</div>
+				<div class="test">
+					<a href="" class="header__img">
+						<img src="img/logo.png" alt="">
+					</a>
+					<ul class="header__list">
+						<li class="header__list-item"><a href="" class="header__list-link">Распродажа</a></li>
+						<li class="header__list-item"><a href="" class="header__list-link">О компании</a></li>
+						<li class="header__list-item">
+							<a href="" class="header__list-link">Каталог</a>
+							<div class="header__lunge-rhombus"></div>
+							<div class="header__lunge">
+									<div class="container">
+										<ul class="header__lunge-list">
+											<li class="header__lunge-main">Столы и стулья</li>
+											<div class="header__lunge-list-flex">
+												<li class="header__lunge-item"><a href="">Стулья</a></li>
+												<li class="header__lunge-item"><a href="">Журнальные столики</a></li>
+												<li class="header__lunge-item"><a href="">Столы</a></li>
+												<li class="header__lunge-item"><a href="">Тумбы</a></li>
+												<li class="header__lunge-item"><a href="">Консоль</a></li>
+												<li class="header__lunge-item"><a href="">Сервировочные столы</a></li>
+												<li class="header__lunge-item"><a href="">Письменные столы</a></li>
+											</div>
+										</ul>
+										<ul class="header__lunge-list">
+											<li class="header__lunge-main">Гостиные</li>
+											<div class="header__lunge-list-flex">
+												<li class="header__lunge-item"><a href="">Наборные системы</a></li>
+												<li class="header__lunge-item"><a href="">Витрины</a></li>
+												<li class="header__lunge-item"><a href="">ТВ тумбы</a></li>
+												<li class="header__lunge-item"><a href="">Прихожие</a></li>
+												<li class="header__lunge-item"><a href="">Стенки</a></li>
+												<li class="header__lunge-item"><a href="">Прихожие</a></li>
+												<li class="header__lunge-item"><a href="">Буфеты</a></li>
+											</div>
+										</ul>
+										<ul class="header__lunge-list">
+											<li class="header__lunge-main">Спальни</li>
+											<div class="header__lunge-list-flex">
+												<li class="header__lunge-item"><a href="">Спальные гарнитуры и кровати</a></li>
+												<li class="header__lunge-item"><a href="">Матрасы</a></li>
+												<li class="header__lunge-item"><a href="">Прикроватные тумбочки</a></li>
+												<li class="header__lunge-item"><a href="">Туалетные столики</a></li>
+												<li class="header__lunge-item"><a href="">Комоды</a></li>
+												<li class="header__lunge-item"><a href="">Письменные столы</a></li>
+											</div>
+										</ul>
+										<ul class="header__lunge-list">
+											<li class="header__lunge-main">Гостиные</li>
+											<div class="header__lunge-list-flex">
+												<li class="header__lunge-item"><a href="">Наборные системы</a></li>
+												<li class="header__lunge-item"><a href="">Витрины</a></li>
+												<li class="header__lunge-item"><a href="">ТВ тумбы</a></li>
+												<li class="header__lunge-item"><a href="">Прихожие</a></li>
+												<li class="header__lunge-item"><a href="">Стенки</a></li>
+												<li class="header__lunge-item"><a href="">Прихожие</a></li>
+												<li class="header__lunge-item"><a href="">Буфеты</a></li>
+											</div>
+										</ul>
+										<ul class="header__lunge-list">
+											<li class="header__lunge-main">Кабинеты</li>
+										</ul>
+									</div>
+							</div>
+						</li>
+						<li class="header__list-item"><a href="" class="header__list-link">Фабрики</a></li>
+						<li class="header__list-item"><a href="" class="header__list-link">Сотрудничество</a></li>
+					</ul>
+				</div>
+				<div class="header__fhone">
+					<a href="" class="header__fhone-link">
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+						<path d="M15 15L21 21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+					</a> 
+					<a href="" class="header__fhone-link">
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M9.50289 4.25722L8.57441 4.62861V4.62861L9.50289 4.25722ZM10.5572 6.89288L11.4856 6.52149V6.52149L10.5572 6.89288ZM10.2366 8.91603L11.0049 9.55621V9.55621L10.2366 8.91603ZM9.66925 9.5969L10.4375 10.2371L9.66925 9.5969ZM9.79148 12.2915L9.08437 12.9986L9.79148 12.2915ZM11.7085 14.2085L12.4156 13.5014L11.7085 14.2085ZM14.4031 14.3307L13.7629 13.5625L14.4031 14.3307ZM15.084 13.7634L15.7242 14.5316L15.7242 14.5316L15.084 13.7634ZM17.1071 13.4428L16.7357 14.3713L17.1071 13.4428ZM19.7428 14.4971L20.1142 13.5686L19.7428 14.4971ZM4.89474 4H7.64593V2H4.89474V4ZM8.57441 4.62861L9.62867 7.26427L11.4856 6.52149L10.4314 3.88583L8.57441 4.62861ZM9.46842 8.27585L8.90103 8.95672L10.4375 10.2371L11.0049 9.55621L9.46842 8.27585ZM9.08437 12.9986L11.0014 14.9156L12.4156 13.5014L10.4986 11.5844L9.08437 12.9986ZM15.0433 15.099L15.7242 14.5316L14.4438 12.9951L13.7629 13.5625L15.0433 15.099ZM16.7357 14.3713L19.3714 15.4256L20.1142 13.5686L17.4785 12.5144L16.7357 14.3713ZM20 16.3541V19.1053H22V16.3541H20ZM19.1053 20C10.7629 20 4 13.2371 4 4.89474H2C2 14.3417 9.65829 22 19.1053 22V20ZM20 19.1053C20 19.5994 19.5994 20 19.1053 20V22C20.704 22 22 20.704 22 19.1053H20ZM19.3714 15.4256C19.751 15.5775 20 15.9452 20 16.3541H22C22 15.1274 21.2531 14.0242 20.1142 13.5686L19.3714 15.4256ZM15.7242 14.5316C16.0063 14.2964 16.3947 14.2349 16.7357 14.3713L17.4785 12.5144C16.4554 12.1051 15.2903 12.2897 14.4438 12.9951L15.7242 14.5316ZM11.0014 14.9156C12.0986 16.0128 13.8513 16.0923 15.0433 15.099L13.7629 13.5625C13.3656 13.8936 12.7813 13.8671 12.4156 13.5014L11.0014 14.9156ZM8.90103 8.95672C7.90771 10.1487 7.98722 11.9014 9.08437 12.9986L10.4986 11.5844C10.1329 11.2187 10.1064 10.6344 10.4375 10.2371L8.90103 8.95672ZM9.62867 7.26427C9.76509 7.6053 9.70356 7.99367 9.46842 8.27585L11.0049 9.55621C11.7103 8.7097 11.8949 7.54459 11.4856 6.52149L9.62867 7.26427ZM7.64593 4C8.05484 4 8.42255 4.24895 8.57441 4.62861L10.4314 3.88583C9.97577 2.74685 8.87265 2 7.64593 2V4ZM4.89474 2C3.29602 2 2 3.29602 2 4.89474H4C4 4.40059 4.40059 4 4.89474 4V2Z" fill="white"/>
+						</svg>
+						+7 (985) 888-15-05
+					</a>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script>
+export default
+{
+}
+</script>
+
+<style lang="scss">
+	ul
+	{
+		list-style-type: none;
+	}
+	.header
+	{
+		background: #313131;
+		height: 75px;
+		display: flex;
+		align-items: center;
+	}
+	.header__wrapper
+	{
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.header__iner
+	{
+		display: flex;
+		column-gap: 15px;
+	}
+	.header__img
+	{
+		position: relative;
+		top: 3px;
+		right: 20px;
+	}
+	.header__list {display: none;}
+	.header__lunge {display: none;}
+	.header__fhone
+	{
+		.header__fhone-link:first-child
+		{
+			display: inline-block;
+			height: 24px;
+		}	
+	}
+	.header__fhone-link {display: none;}
+	@media (min-width: 768px)
+	{
+		.header {height: 85px;}
+		.container {padding: 0 10px;}
+		.header__iner {display: none;}
+		.test
+		{
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			max-width: 823px;
+			width: 100%;
+		}
+		.header__img
+		{
+			position: static;
+			height: 45px;
+			width: 99px;
+			margin-left: 14px;;
+			img
+			{
+				height: 45px;
+				width: 99px;
+			}
+		}
+		.header__list
+		{
+			display: flex;
+			margin: 0;
+			padding: 0;
+			max-width: 613px;
+		    width: 100%;
+		    justify-content: space-between;
+		    margin-left: 15px;
+			.header__list-item
+			{
+				position: relative;
+				line-height: 85px;
+				.header__list-link
+				{
+					position: relative;
+					text-decoration: none;
+					font-family: 'Open Sans';
+					font-style: normal;
+					font-weight: 400;
+					font-size: 14px;
+					line-height: 19px;
+					color: #FFFFFF;
+					&:hover { color: rgba(255, 255, 255, 0.6); }
+				}
+			}
+		}
+		.header__lunge
+		{
+			display: none;
+			position: fixed;
+			color: red;
+			width: 100%;
+			height: 339px;
+		    left: 0;
+		    top: 85px;
+		    padding-top: 20px;
+		    background: #F8F8F8;
+		    .container
+			{
+				padding: 0 20px;
+			}
+		}
+		.header__lunge-rhombus
+		{
+			display: none;
+			width: 15.34px;
+			height: 15.34px;
+			background: #F8F8F8;
+			transform: rotate(-45deg);
+			position: absolute;
+			top: 79px;
+			left: 18px;
+		}
+		.header__list-item:hover .header__lunge-rhombus
+		{
+			display: block;
+		}
+		.header__list-item:hover .header__lunge
+		{
+			display: block;
+		}
+		.header__lunge-list
+		{
+			padding: 0;
+			margin-bottom: 13px
+		}
+		.header__lunge-main
+		{
+			font-family: 'Open Sans';
+			font-style: normal;
+			font-weight: 700;
+			font-size: 14px;
+			line-height: 19px;
+			color: #313131;
+			margin-bottom: 5px;
+		}
+		.header__lunge-list-flex
+		{
+			display: flex;
+			column-gap: 18px;
+		}
+		.header__lunge-item
+		{
+			list-style-type: none;
+			line-height: 0;
+			position: static;
+			a
+			{
+				text-decoration: none;
+				font-family: 'Open Sans';
+				font-style: normal;
+				font-weight: 400;
+				font-size: 14px;
+				line-height: 27px;
+				color: #313131;
+				&:hover {border-bottom: 1px solid rgba(49, 49, 49, 0.6);}
+			}
+		}
+		.header__fhone
+		{
+			display: flex;
+			align-items: center;
+			column-gap: 18px;
+			max-width: 222px;
+			width: 100%;
+			margin-left: 15px;
+		}
+		.header__fhone-link
+		{
+			display: flex;
+			align-items: center;
+			column-gap: 8px;
+			text-decoration: none;
+			font-family: 'Open Sans';
+			font-style: normal;
+			font-weight: 600;
+			font-size: 16px;
+			line-height: 22px;
+			color: #FFFFFF;
+			&:hover { color: rgba(255, 255, 255, 0.6); };
+		}
+	}
+	@media (max-width: 1024px)
+	{
+		.header__list
+		{
+			.header__list-item
+			{
+				.header__list-link
+				{
+					font-size: 12px;
+				}
+			}
+		}
+		.header__fhone-link {font-size: 14px;}
+		.header__lunge-main 
+		{
+			font-size: 12px;
+		}
+		.header__lunge-item
+		{
+			a
+			{
+				font-size: 11px;
+			}			
+		}
+	}
+</style>
